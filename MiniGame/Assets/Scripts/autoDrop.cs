7a592811420c358edcb6025e0de8d26e454a1d2e
Coldle <1914999099@qq.com>
2020-10-01 11:52:39 +0800

    First push

---
+++ MiniGame/Assets/Scripts/autoDrop.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System.Linq;
+
+//测试脚本
+public class autoDrop : MonoBehaviour
+{
+    public Transform startTrans;
+    public Transform midTrans;
+    public Transform endTrans;
+    public Transform thing;
+    //public Transform[] PointList;//生成路径的保存的目标点
+
+    public float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1)
+            timer = 0;
+        Drop();
+    }
+
+    public void Drop()
+    {
+
+        thing.position = Bezier(startTrans.position, midTrans.position, endTrans.position, (float)Mathf.Sqrt(timer));
+        // for (int i = 0; i < PointList.Length; i++)
+        // {
+        //     PointList[i].position = Bezier(startTrans.position, midTrans.position, endTrans.position, (float)((i + 1) / 10.0));
+        // }
+        // gameObject.transform.position = PointList[0].position;//物品位置更新为第一个目标点(保证相对玩家的轨迹正常)
+        // var positions = PointList.Select(u => u.position).ToArray();
+        // transform.DOPath(positions, 10, PathType.CatmullRom, 0, 10);//(目标点, 运动总耗时, 运动方式:曲线, Lookat, 轨迹精度)
+    }
+
+    //绘制贝塞尔曲线作为路径
+    Vector3 Bezier(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        Vector3 p0p1 = (1 - t) * p0 + t * p1;
+        Vector3 p1p2 = (1 - t) * p1 + t * p2;
+        Vector3 result = (1 - t) * p0p1 + t * p1p2;
+        return result;
+    }
+
+}
---
+++ MiniGame/Assets/Scripts/foodQuantity.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//需要修改, 饥饿值与数量无关(每一小段表示一只老鼠)
+//测试用, 数值全都是偏高偏快的赋值
+public class foodQuantity : MonoBehaviour
+{
+    public int number;//种群数量
+    public float health = 100;//食物数量?饥饿值?健康度?
+    public float timer = 2.0f;
+    public Transform processTrans;//进度条
+
+    public Transform indicatorTrans;//文字框
+
+    void Update()
+    {
+        HealthAutoReduction();
+    }
+
+    public void HealthAutoReduction()
+    {
+        health -= (float)(0.001 * number);
+        indicatorTrans.GetComponent<Text>().text = ((int)health).ToString() + "%";
+        processTrans.GetComponent<Image>().fillAmount = health / 100.0f;
+        if (health >= 30 && health < 50)
+        {
+            timer -= 2 * Time.deltaTime;
+        }
+
+        if (health >= 10 && health < 30)
+        {
+            timer -= 5 * Time.deltaTime;
+        }
+
+        if (health < 10)
+        {
+            timer -= 10 * Time.deltaTime;
+        }
+
+        if (health <= 0)
+        {
+            health = 0;
+            //GameOver();
+        }
+
+        if (timer <= 0)
+        {
+            number -= (int)(0.1 * number);
+            timer = 2.0f;
+        }
+
+    }
+}
