ad575e5591ae459f324f0b48e2e31fbdbf786392
Coldle <1914999099@qq.com>
2020-10-11 14:02:46 +0800

    BackpackScale v0.2

--- MiniGame/Assets/Scripts/foodQuantity.cs
+++ MiniGame/Assets/Scripts/foodQuantity.cs
@@ -27,18 +27,18 @@ public class foodQuantity : MonoBehaviour
     }
     public void HealthAutoReduction()
     {
-        health -= Time.deltaTime * (float)System.Math.Sqrt(number);
-        timer -= Time.deltaTime * (float)System.Math.Sqrt(number);
+        // health -= Time.deltaTime * (float)System.Math.Sqrt(number);
+        // timer -= Time.deltaTime * (float)System.Math.Sqrt(number);
 
-        if (timer < 9.0f)
-        {
-            Debug.Log(timer);
-            for (int i = 0; i < (int)(100.0f - timer); i++)
-                BackpackManager.RemoveItem();
-            Debug.Log((int)(100.0f - timer));
-            timer = 100.0f;
-        }
-        indicatorTrans.GetComponent<Text>().text = ((int)health).ToString() + "%";
-        processTrans.GetComponent<Image>().fillAmount = health / 100.0f;
+        // if (timer < 9.0f)
+        // {
+        //     Debug.Log(timer);
+        //     for (int i = 0; i < (int)(100.0f - timer); i++)
+        //         BackpackManager.RemoveItem();
+        //     Debug.Log((int)(100.0f - timer));
+        //     timer = 100.0f;
+        // }
+        // indicatorTrans.GetComponent<Text>().text = ((int)health).ToString() + "%";
+        // processTrans.GetComponent<Image>().fillAmount = health / 100.0f;
     }
 }
