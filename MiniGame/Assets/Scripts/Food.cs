5a5053049b18ed9c4a89dfd451ba824a000139bc
magicianA <a1awsdadsdasd@126.com>
2020-10-19 21:14:39 +0800

    猫头鹰

---
+++ MiniGame/Assets/Scripts/Food.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : Item
+{
+    //补充的饥饿值
+    public int deltaHungryValue;
+    public override void use()
+    {
+        BackpackManager.instance.gridNum += deltaHungryValue;
+        //LemmingSumControl._Instance.LemmingNumTrue += hungryValue;
+    }
+}
